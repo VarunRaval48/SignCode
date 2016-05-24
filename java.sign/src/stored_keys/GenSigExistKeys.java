20b742696f64b0714a75a1d6979f9c105069da56
Varun Raval <vraval48@gmail.com>
2016-05-24 20:38:24 +0530

    update to test individual operations

--- java.sign/src/stored_keys/GenSigExistKeys.java
+++ java.sign/src/stored_keys/GenSigExistKeys.java
@@ -18,25 +18,36 @@ import java.security.cert.CertificateException;
 public class GenSigExistKeys {
 
 	//TODO Enter valid pass here
-	private static final String keyStorePass="";		//password of keystore
-	private static final String privateKeyPass="";		//password of privatekey
+	private static final String keyStorePass="9va9r9un123";		//password of keystore
+	private static final String privateKeyPass="9va9r9un123";		//password of privatekey
 
 	public static void main(String args[]){
-		
+
 		String pathToKeyStore = "/home/varun/keystore";
-		String dataFile="/home/varun/Documents/TODO", signFile="/home/varun/Documents/TODO_sign_exist_1.sgn";
+		String dataFile="/home/varun/Documents/projects/GSoC/T1_testData/files/hello.js", 
+//				signFile="/home/varun/Documents/projects/GSoC/T1_testData/signed_files/TODO_sign_exist_1.sgn",
+//				signStrFile="/home/varun/Documents/projects/GSoC/TODO_sign_exist_1_str.txt";
+		signFile="/home/varun/Documents/projects/GSoC/T1_testData/signed_files/hello_sign_exist.sgn",
+		signStrFile="/home/varun/Documents/projects/GSoC/T1_testData/signed_files/hello_sign_exist_str.js";
+
 		try {
-		
+
 			KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-			
+
 			FileInputStream fileInputStream = new FileInputStream(new File(pathToKeyStore));
-			
+
 			keyStore.load(fileInputStream, keyStorePass.toCharArray());
-		
+
 			GetSigExistingKeys gsek = new GetSigExistingKeys();
-			
-			gsek.sigUsingExistKeys(keyStore, "Varun Raval", privateKeyPass, dataFile, signFile);
-			
+
+			byte[] realsig = gsek.sigUsingExistKeys(keyStore, "Varun Raval", privateKeyPass, dataFile, signFile);
+
+			gsek.storeSignUsingExistKeys(realsig, signFile);
+
+			String strSig = gsek.convertBytesToBase64(realsig);
+
+			gsek.storeSignUsingExistKeys(strSig, signStrFile);
+
 		} catch (KeyStoreException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
