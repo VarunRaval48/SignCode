b867a5ae2d5ba3fbc731eb6409fe86ee2de8bb43
Varun Raval <vraval48@gmail.com>
2016-05-26 20:55:34 +0530

    Certificate can be attached

--- java.sign/src/test_data/GenTestScript.java
+++ java.sign/src/test_data/GenTestScript.java
@@ -13,20 +13,26 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
 
+import signature.CertImport;
 import stored_keys.GetSigExistingKeys;
 import stored_keys.LoadKeystore;
 
 public class GenTestScript {
 
+	
 	public static void main(String args[]){
 		
 		String dataFile = "/home/varun/Documents/projects/GSoC/T1_testData/files/hello.js", 
 				signFile = "/home/varun/Documents/projects/GSoC/T1_testData/signed_files/hello.js",
 				pathToKeyStore = "/home/varun/keystore",
-				keyStorePass = "9va9r9un123",
+				keyStorePass = "",
 				alias = "Varun Raval",
-				privateKeyPass = "9va9r9un123";
+				privateKeyPass = "";
+
+		Certificate certificate = null;
 		
 		GetSigExistingKeys gsek = new GetSigExistingKeys();
 
@@ -36,15 +42,30 @@ public class GenTestScript {
 		try {
 		
 			realsig = gsek.sigUsingExistKeys(keyStore, alias, privateKeyPass, dataFile, signFile);
+
+			CertImport certImport = new CertImport(keyStore);
+			
+			certificate = certImport.importCert(alias);
 			
 		} catch (UnrecoverableKeyException | InvalidKeyException | KeyStoreException | NoSuchAlgorithmException
 				| NoSuchProviderException | SignatureException e) {
 
 			e.printStackTrace();
+			return;
 		}
 
 		String strSig = gsek.convertBytesToBase64(realsig);
 
+		byte[] certByte;
+		try {
+			certByte = certificate.getEncoded();
+		} catch (CertificateEncodingException e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		String strCert = gsek.convertBytesToBase64(certByte);
+		
 		try(BufferedWriter brW = new BufferedWriter(new FileWriter(signFile))){
 
 			BufferedReader br = new BufferedReader(new FileReader(dataFile));
@@ -60,6 +81,8 @@ public class GenTestScript {
 			
 			brW.append("/********BEGIN SIGNATURE********\n");
 			brW.append(strSig);
+			brW.append("\n\n");
+			brW.append(strCert);
 			brW.append("\n********END SIGNATURE********/");
 
 		} catch (FileNotFoundException e) {
