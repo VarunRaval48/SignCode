b0e3ada436c471dc5cdb1761c1e5d8ede6c010a5
Varun Raval <vraval48@gmail.com>
2016-06-03 11:04:14 +0530

    Added comments

--- java.sign/src/test_data/GenTestScript.java
+++ java.sign/src/test_data/GenTestScript.java
@@ -43,13 +43,13 @@ public class GenTestScript {
 
 		KeyStore keyStore = LoadKeystore.load(pathToKeyStore, keyStorePass);
 		
+		//perform signature and get Certificate in Certificate object from keystore
 		byte[] realsig = null;
 		try {
 		
 			realsig = gsek.sigUsingExistKeys(keyStore, alias, privateKeyPass, dataFile, signFile);
 
-			CertImport certImport = new CertImport(keyStore);
-			
+			CertImport certImport = new CertImport(keyStore);			
 			certificate = certImport.importCert(alias);
 			
 		} catch (UnrecoverableKeyException | InvalidKeyException | KeyStoreException | NoSuchAlgorithmException
@@ -59,8 +59,10 @@ public class GenTestScript {
 			return;
 		}
 
+		//convert signature to base64
 		String strSig = gsek.convertBytesToBase64(realsig);
 
+		//get certificate in bytes format
 		byte[] certByte;
 		try {
 			certByte = certificate.getEncoded();
@@ -69,8 +71,10 @@ public class GenTestScript {
 			return;
 		}
 
+		//convert cert bytes to base64
 		String strCert = gsek.convertBytesToBase64(certByte);
-		
+
+		//Appending Signature and Certificate
 		try(BufferedWriter brW = new BufferedWriter(new FileWriter(signFile))){
 
 			BufferedReader br = new BufferedReader(new FileReader(dataFile));
@@ -82,11 +86,13 @@ public class GenTestScript {
 			
 			br.close();
 
+			//To enter new line after last line of script and bring pointer to next line
 			brW.append("\n\n");
 
 			String ext = (String)signFile.subSequence(signFile.lastIndexOf(".")+1, signFile.length());
 			System.out.println(ext);
 
+			//Each line is terminated by '\n'
 			switch(ext){
 		
 			case "js":
@@ -97,12 +103,11 @@ public class GenTestScript {
 				end = "\n********END SIGNSTURE*****\"\"\"";
 				break;
 			}
-				
-			
+
 			brW.append(begin);
 			brW.append(strSig);
 			brW.append("\n\n");
-			
+
 			int i = 0;
 			for(String s: strCert.split("")){
 				brW.append(s);
@@ -110,7 +115,7 @@ public class GenTestScript {
 				if(i%48 == 0)
 					brW.append('\n');
 			}
-			
+
 			brW.append(end);
 
 		} catch (FileNotFoundException e) {
