a18429fcd4544c9422aea2d5c4182a3abb25bfd9
Varun Raval <vraval48@gmail.com>
2016-06-21 18:55:12 +0530

    get end content using normal method using stream with matcher

--- java.sign/src/testing/SignPatternTest.java
+++ java.sign/src/testing/SignPatternTest.java
@@ -9,23 +9,23 @@ public class SignPatternTest {
 
 	public static void main(String args[]) throws Exception {
 
-		BufferedInputStream bufferedInputStream = new BufferedInputStream(
+		BufferedInputStream bStream = new BufferedInputStream(
 				new FileInputStream("/home/varun/Documents/projects/GSoC/T1_testData/signed_files/hello.js"));
 
 		int c;
 		StringBuffer strBuf = new StringBuffer();
-		while ((c = bufferedInputStream.read()) != -1) {
+		while ((c = bStream.read()) != -1) {
 			strBuf.append((char) c);
 		}
 
-		/*Signature to be tested must be in this form
+		/*Signature to be tested must be in this format
 
 		beginCommentString
 		Hash:hash Provider:provider
 
 		signature(64)bytes
 
-		certificate(multiple lines, each line having 48 bytes)(certificate separated by :(colon) )
+		certificate(multiple lines, each line having 48 bytes except last line)(certificates separated by :(colon) )
 
 		endCommentString
 
@@ -33,15 +33,15 @@ public class SignPatternTest {
 		For signature only: \n.+\n-----BEGIN SIGNATURE-----\n(Hash:[\w^_]+ Provider:[\w^_]+)\n\n([\w+/=^_]{64})\n\n(([\w+/=^_:]{48}\n)+[\w+/=^_:]*)\n\n-----END SIGNATURE-----\n.+
 		For contents ahead: ([\s\S]+)(?=(\n.+\n-----BEGIN SIGNATURE-----\n(Hash:[\w^_]+ Provider:[\w^_]+)\n\n([\w+/=^_]{64})\n\n(([\w+/=^_:]{48}\n)+[\w+/=^_:]*)\n\n-----END SIGNATURE-----\n.+))
 		 */
-		// String pattern = "(?<=(\n.+\n-----BEGIN SIGNATURE-----\n(Hash:[\\w^_]+ Provider:[\\w^_]+)\n\n([\\w+/=^_]{64})\n\n(([\\w+/=^_:]{48}\n)+[\\w+/=^_:]*)\n\n-----END SIGNATURE-----\n.+\n))";
+		// String pattern = "(\n.+\n-----BEGIN SIGNATURE-----\n(Hash:[\\w^_]+ Provider:[\\w^_]+)\n\n([\\w+/=^_]{64})\n\n(([\\w+/=^_:]{48}\n)+[\\w+/=^_:]*)\n\n-----END SIGNATURE-----\n.+\n)";
 
-		String pattern = "([\\s\\S]+)(?=(\n.+\n-----BEGIN SIGNATURE-----\n(Hash:[\\w^_]+ Provider:[\\w^_]+)\n\n([\\w+/=^_]{64})\n\n(([\\w+/=^_:]{48}\n)+[\\w+/=^_:]*)\n\n-----END SIGNATURE-----\n.+\n))";
+		String pattern = "([\\s\\S]*)(?=(\n.+\n-----BEGIN SIGNATURE-----\n(Hash:[\\w^_]+ Provider:[\\w^_]+)\n\n([\\w+/=^_]{64})\n\n(([\\w+/=^_:]{48}\n)+[\\w+/=^_:]*)\n\n-----END SIGNATURE-----\n.+\n))";
 
 		Pattern r = Pattern.compile(pattern);
 		Matcher m = r.matcher(strBuf.toString());
 
-		System.out.println(m.find());
-		System.out.println(m.groupCount());
+		System.out.print(m.find());
+		//		System.out.println(m.groupCount());
 
 		String contentAhead = m.group(1);
 		String signBlock = m.group(2);
@@ -53,8 +53,27 @@ public class SignPatternTest {
 		System.out.println(signBlock);
 		System.out.println(signParam);
 		System.out.println(signature);
-		System.out.println(certificate);
+		System.out.print(certificate);
 
-		bufferedInputStream.close();
+		bStream.close();
+
+		bStream = new BufferedInputStream(
+				new FileInputStream("/home/varun/Documents/projects/GSoC/T1_testData/signed_files/hello.js"));
+
+		int i = 0, end = m.end(2);
+		do {
+			bStream.read();
+			i++;
+		} while (i < end - 1);
+
+		StringBuffer cBuffer = new StringBuffer();
+
+		while ((c = bStream.read()) != -1) {
+			cBuffer.append((char) c);
+		}
+
+		String endContent = cBuffer.toString();
+
+		System.out.print(endContent);
 	}
 }
